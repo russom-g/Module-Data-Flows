21524fb0f26b02057cb4966ef71a731db1d7599f
russom <russom.g.meskel@gmail.cpm>
2026-08-06 12:34:14 +0100

    Log items logged with no formatting first

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order) {
+  order.forEach(({ quantity, itemName, unitPricePence }) => {
+    console.log(`${quantity} ${itemName} ${unitPricePence}`);
+  });
+}
+
+printReceipt(order);
+// 1. Log each item quantity and name.
+// 2. Log total cost.
+// 3. 
